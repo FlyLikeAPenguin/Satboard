c22831ff3885b60047cecfe47739751981d62948
Joe Thornton <joe.clifford.thornton@gmail.com>
2025-06-08 14:11:39 +0100

    Updates

--- data/tle/get-norad.sh
+++ data/tle/get-norad.sh
@@ -7,9 +7,9 @@ mkdir -p norad && cd norad
 # https://celestrak.org/NORAD/elements/
 # [...document.links].forEach(link => {if (link.href.match(/txt$/)) {console.log(link.href)}});
 
-curl -O https://celestrak.org/NORAD/elements/tle-new.txt
-curl -O https://celestrak.org/NORAD/elements/stations.txt
-curl -O https://celestrak.org/NORAD/elements/visual.txt
+curl "https://celestrak.org/NORAD/elements/gp.php?GROUP=last-30-days&amp;FORMAT=tle" -o tle-new.txt
+curl "https://celestrak.org/NORAD/elements/gp.php?GROUP=stations&amp;FORMAT=tle" -o stations.txt
+curl "https://celestrak.org/NORAD/elements/gp.php?GROUP=visual&amp;FORMAT=tle" -o visual.txt
 # curl -O https://celestrak.org/NORAD/elements/active.txt
 curl "https://celestrak.org/NORAD/elements/gp.php?CATNR=43890&FORMAT=tle" -o grus.txt
 curl "https://celestrak.org/NORAD/elements/gp.php?CATNR=47933&FORMAT=tle" >> grus.txt
